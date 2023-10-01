753a58d04b1b9e5f00cbf31f8a1c919a09d3aec2
Jung-Uiseok <tnekwjd3359@naver.com>
2023-10-01 19:11:51 +0900

    draw hand_arrow when click

--- drill06.py
+++ drill06.py
@@ -9,26 +9,39 @@ hand_arrow = load_image('hand_arrow.png')
 
 def handle_events():
     global running
-    global x, y
+    global x, y, click, click_x, click_y
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
             x, y = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            if event.button == SDL_BUTTON_LEFT:
+                click = True
+                click_x, click_y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
+click = False
+click_x = 0
+click_y = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, TUK_WIDTH//2, TUK_HEIGHT//2)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, TUK_WIDTH // 2, TUK_HEIGHT // 2)
     hand_arrow.draw(x, y)
+
+    if click:
+        hand_arrow.draw(click_x, click_y)
+
     update_canvas()
     frame = (frame + 1) % 8
 
